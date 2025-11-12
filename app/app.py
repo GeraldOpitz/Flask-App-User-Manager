6ded07a38142f70155ccfd12a372e8ff86c9b21e
GeraldOpitz <geraldopitz@gmail.com>
2025-11-12 17:31:46 -0300

    FUM-70 Hotfix: Added tablename users

--- app/app.py
+++ app/app.py
@@ -22,6 +22,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
